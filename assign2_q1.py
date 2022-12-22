f51b658d55d1c3bcd9e62fe1537ec3cf516c568d
bt22103040DhruvSingal <120629372+bt22103040DhruvSingal@users.noreply.github.com>
2022-12-22 14:24:40 +0530

    Add files via upload

---
+++ assign2_q1.py
@@ -0,0 +1,11 @@
+x="python is a case sensitive language"
+print("length of string=",len(x))
+print(x[::-1])
+b=x[10:26:1]
+print(b)
+replaced=x.replace("a case sensitive","object oriented")
+print(replaced)
+print(x.index("a"))
+print(x.replace(" ",""))
+
+
